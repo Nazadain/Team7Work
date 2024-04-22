d35c9be39fba6200da7ab0371ca8027c425d53a8
Nazadain <oldfather3@mail.ru>
2024-04-22 22:21:34 +0500

    На сегодня всё

--- client/script.js
+++ client/script.js
@@ -1,11 +1,11 @@
 let flag = 0;
 function OpenCloseBasket() {
     if (flag == 0) {
-        document.getElementById("basketToggle").style.display = 'block';
+        document.getElementById("basketToggle").classList.add("show");
         flag = 1;
     }
     else {
-        document.getElementById("basketToggle").style.display = 'none';
+        document.getElementById("basketToggle").classList.remove("show");
         flag = 0;
     }
 }
