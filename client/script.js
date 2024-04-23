31a19a077b7d153482de8cf9b06d0fe1c48203f5
Nazadain <oldfather3@mail.ru>
2024-04-23 10:33:39 +0500

    basket close by esc and click on window

--- client/script.js
+++ client/script.js
@@ -1,11 +1,22 @@
-let flag = 0;
-function OpenCloseBasket() {
-    if (flag == 0) {
-        document.getElementById("basketToggle").classList.add("show");
-        flag = 1;
+let basket = document.querySelector( '#basketToggle');
+let basketButton = document.querySelector( '#basketButton');
+document.addEventListener( 'click', (e) => {
+	let insideMenu = e.composedPath().includes(basket);
+    let insideButton = e.composedPath().includes(basketButton);
+	if(insideButton) {
+        if(!basket.classList.contains("show")) {
+            basket.classList.add("show");
+        }
+        else {
+            basket.classList.remove("show");
+        }
     }
-    else {
-        document.getElementById("basketToggle").classList.remove("show");
-        flag = 0;
+    else if(! insideMenu && basket.classList.contains("show")) {
+        basket.classList.remove("show");
     }
-}
+})
+document.addEventListener('keydown', function(e) {
+	if( e.keyCode == 27 && basket.classList.contains("show")){ // код клавиши Escape, но можно использовать e.key
+		basket.classList.remove("show");
+	}
+});
