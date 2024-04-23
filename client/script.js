87e605c030a51705faedcf5d0ac37a8ab84fe04c
Nazadain <oldfather3@mail.ru>
2024-04-23 16:14:33 +0500

    cart in progress

--- client/script.js
+++ client/script.js
@@ -1,27 +1,97 @@
-let basket = document.querySelector( '#basketToggle');
-let basketButton = document.querySelector( '#basketButton');
+let cartMonitor = document.querySelector( '#cartToggle');
+let cartButton = document.querySelector( '#cartButton');
 let blackFade = document.querySelector('#blackFade');
+let cart = {
+    'pizz001' : {
+        "name" : "Салями",
+        "price" : 589,
+        "count" : 0,
+    },
+    'pizz002' : {
+        "name" : "Оливковая",
+        "price" : 699,
+        "count" : 0,
+    },
+    'pizz003' : {
+        "name" : "Песто",
+        "price" : 899,
+        "count" : 0,
+    },
+    'roll001' : {
+        "name" : "Калифорния",
+        "price" : 399,
+        "count" : 0,
+    },
+    'roll002' : {
+        "name" : "Филадельфия",
+        "price" : 289,
+        "count" : 0,
+    },
+    'roll003' : {
+        "name" : "Маки",
+        "price" : 189,
+        "count" : 0,
+    },
+    'dess001' : {
+        "name" : "Тирамису",
+        "price" : 199,
+        "count" : 0,
+    },
+    'dess002' : {
+        "name" : "Чизкейк",
+        "price" : 199,
+        "count" : 0,
+    },
+    'dess003' : {
+        "name" : "Моти",
+        "price" : 189,
+        "count" : 0,
+    }
+}
+let sum = 0;
 document.addEventListener( 'click', (e) => {
-	let insideMenu = e.composedPath().includes(basket);
-    let insideButton = e.composedPath().includes(basketButton);
+	let insideMenu = e.composedPath().includes(cartMonitor);
+    let insideButton = e.composedPath().includes(cartButton);
 	if(insideButton) {
-        if(!basket.classList.contains("show")) {
-            basket.classList.add("show");
+        if(!cartMonitor.classList.contains("show")) {
+            cartMonitor.classList.add("show");
             blackFade.style.display = 'block';
         }
         else {
-            basket.classList.remove("show");
+            cartMonitor.classList.remove("show");
             blackFade.style.display = 'none';
         }
     }
-    else if(! insideMenu && basket.classList.contains("show")) {
-        basket.classList.remove("show");
+    else if(! insideMenu && cartMonitor.classList.contains("show")) {
+        cartMonitor.classList.remove("show");
         blackFade.style.display = 'none';
+    }
+    else if(e.target.classList.contains('plus')) {
+        plusFunction(e.target.dataset.id);
+    }
+    else if(e.target.classList.contains('minus')) {
+        minusFunction(e.target.dataset.id);
     }
 })
 document.addEventListener('keydown', function(e) {
-	if( e.keyCode == 27 && basket.classList.contains("show")){
-		basket.classList.remove("show");
+	if( e.keyCode == 27 && cartMonitor.classList.contains("show")){
+		cartMonitor.classList.remove("show");
         blackFade.style.display = 'none';
 	}
 });
+
+function plusFunction(id) {
+    cart[id]["count"]++;
+    sum += cart[id]["price"];
+    console.log(sum);
+    renderCart();
+}
+
+function minusFunction(id) {
+    cart[id]["count"]--;
+    renderCart();
+}
+
+function renderCart() {
+    console.log(cart);
+}
